d1a9003078ef1d55b1abbf60a5f15f874b3af70c
Raquel Costa <raquel.moreira.costa@saint-gobain.com>
2025-05-20 23:14:03 -0300

    iniciando login

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import React, { useEffect, useState } from 'react';
+
 import './App.css'
+import Login from './componentes/Login';
 
 
 function App() {
-  const [usuarios, setUsuarios] = useState([]);
-  useEffect(() => {
-    // Requisição GET para listar usuários
-    fetch('http://localhost:8080/usuarios')
-      .then(response => response.json()) // converte a resposta para JSON
-      .then(data => setUsuarios(data))    // armazena os dados no estado
-      .catch(error => console.error('Erro ao buscar usuários:', error));
-  }, []); // O useEffect roda apenas uma vez quando o componente é montado
-
 
   return (
     <>
-      <div>
-        <h1>Lista de Usuários</h1>
-        <ul>
-          {usuarios.map(usuario => (
-            <li key={usuario.id}>{usuario.nome}</li>
-          ))}
-        </ul>
-      </div>
+      <Login />
     </>
   )
 }
