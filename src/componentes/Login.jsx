506609f08d09b9979bfe2cd11572389f1f317716
Atila246 <thalitinha246@gmail.com>
2025-06-07 23:07:47 -0300

    Login e Cadastro finalizados

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,10 +1,9 @@
 import Styles from '../css/Login.module.css'
 import LogoSG from '../assets/img/LogoSG.png'
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Login() {
-
-    //-------------- FUCIONAMENTO DO FORMS --------------
+    //-------------- FUNCIONAMENTO DO FORMS --------------
     const [form, setForm] = useState({
         nomeCompleto: '',
         nomeUsuario: '',
@@ -21,35 +20,54 @@ function Login() {
         console.log('Dados cadastrados:', form);
     };
 
+    //-------------- ANIMAÇÃO --------------
+    const [modo, setModo] = useState("");
+    const [classeAtual, setClasseAtual] = useState("");
+    useEffect(() => {
+        console.log("Modo atual:", modo);
+        if(modo=="signin"){
+            setClasseAtual(Styles.cadastra);
+            console.log("Classe atual:", classeAtual);
+        }
+        if(modo=="signup"){
+            setClasseAtual(Styles.loga);
+        }
+    }, [modo]);
+
     return (
         <>
+            {/*-------------------------- CONTAINER DE FUNDO --------------------------*/}
+            <div className={`${Styles.container} ${classeAtual}`}>
 
-            {/*-------------------------- CONTAINERZÃO DE FUNDO --------------------------*/}
-            <div className={Styles.container}>
-
-                {/*-------------------------- PRIMEIRO ESTADO (CONTENT) - CADASTRO --------------------------*/}
-                <div className={Styles.primeiro_content}>
-
+                {/*-------------------------- PRIMEIRO ESTADO (CADASTRO) --------------------------*/}
+                <div className={`${Styles.content} ${Styles.primeiro_content}`}>
+                    {/* PRIMEIRA COLUNA */}
                     <div className={Styles.primeira_coluna}>
                         <figure className={Styles.logo_login}>
                             <img src={LogoSG} alt="" />
                         </figure>
-                        <h2 className={Styles.primeiro_titulo}>Prazer em conhecê-lo!</h2>
-                        <p className={Styles.primeiro_meio}>Crie seu perfil para participar de comunidades conosco</p>
+                        <h2 className={`${Styles.primeiro_titulo} ${Styles.titulo}`}>Prazer em conhecê-lo!</h2>
+                        <p className={`${Styles.descricao} ${Styles.meio}`}>Crie seu perfil para participar de comunidades conosco</p>
                         <div className={Styles.botao_texto}>
-                            <p className={Styles.primeira_descricao}>Já tem uma conta?</p>
-                            <button className={Styles.botao_entrar}>Entrar</button>
+                            <p className={Styles.descricao}>Já tem uma conta?</p>
+                            <button
+                                className={`${Styles.primeiro_botao} ${Styles.botao}`}
+                                onClick={() => setModo("signup")}
+                                id="signup">Entrar</button>
                         </div>
                     </div>
 
-
+                    {/* SEGUNDA COLUNA */}
                     <div className={Styles.segunda_coluna}>
-
-                        <h2>Crie sua conta</h2>
-
-                        <form>
-
+                        <h2 className={`${Styles.segundo_titulo} ${Styles.titulo}`}>Crie sua conta</h2>
+                        <div className={Styles.social_media}>
+                            <a href="#"><i className="fi fi-brands-google"></i></a>
+                            <a href="#"><i className="fi fi-brands-instagram"></i></a>
+                            <a href="#"><i className="fi fi-brands-google"></i></a>
+                        </div>
+                        <form className={Styles.form}>
                             <label>
+                                <i className="far fa-user icon-modify"></i>
                                 <input
                                     type="text"
                                     name="nomeCompleto"
@@ -58,8 +76,8 @@ function Login() {
                                     onChange={handleChange}
                                 />
                             </label>
-
                             <label>
+                                <i className="fi fi-br-at icon-modify"></i>
                                 <input
                                     type="text"
                                     name="nomeUsuario"
@@ -68,8 +86,8 @@ function Login() {
                                     onChange={handleChange}
                                 />
                             </label>
-
                             <label>
+                                <i className="fi fi-rr-envelope icon-modify"></i>
                                 <input
                                     type="email"
                                     name="email"
@@ -78,8 +96,8 @@ function Login() {
                                     onChange={handleChange}
                                 />
                             </label>
-
                             <label>
+                                <i className="fi fi-sr-lock icon-modify"></i>
                                 <input
                                     type="password"
                                     name="senha"
@@ -88,17 +106,66 @@ function Login() {
                                     onChange={handleChange}
                                 />
                             </label>
-
-                            <button className={Styles.cadastrese} type="button" onClick={handleSubmit}>
+                            <div className={Styles.terms_container}>
+                                <input className={Styles.input_terms} type="checkbox" name="terms" />
+                                <label className={Styles.label_terms} htmlFor="terms">
+                                    Li e aceito os <a href="#" target="_blank">Termos de Uso</a>
+                                </label>
+                            </div>
+                            <button
+                                className={`${Styles.segundo_botao} ${Styles.botao}`}
+                                type="button"
+                                onClick={handleSubmit}
+                            >
                                 Cadastrar-se
                             </button>
                         </form>
                     </div>
                 </div>
 
+                {/*-------------------------- SEGUNDO ESTADO (LOGIN) --------------------------*/}
+                <div className={`${Styles.content} ${Styles.segundo_content}`}>
+                    <div className={Styles.primeira_coluna}>
+                        <figure className={Styles.logo_login}>
+                            <img src={LogoSG} alt="" />
+                        </figure>
+                        <h2 className={`${Styles.primeiro_titulo} ${Styles.titulo}`}>Bem-vindo de volta!</h2>
+                        <p className={`${Styles.descricao} ${Styles.meio}`}>Conecte-se para continuar explorando</p>
+                        <div className={Styles.botao_texto}>
+                            <p className={Styles.descricao}>Ainda não tem uma conta?</p>
+                            <button
+                                className={`${Styles.primeiro_botao} ${Styles.botao}`}
+                                onClick={() => setModo("signin")}
+                                id="signin"
+                            >
+                                Cadastrar
+                            </button>
+                        </div>
+                    </div>
+                    <div className={Styles.segunda_coluna}>
+                        <h2 className={`${Styles.segundo_titulo} ${Styles.titulo}`}>Entrar na sua conta</h2>
+                        <div className={Styles.social_media}>
+                            <a href="#"><i className="fi fi-brands-google"></i></a>
+                            <a href="#"><i className="fi fi-brands-instagram"></i></a>
+                            <a href="#"><i className="fi fi-brands-google"></i></a>
+                        </div>
+                        <form className={Styles.form}>
+                            <label>
+                                <i className="far fa-user icon-modify"></i>
+                                <input type="email" name="email" placeholder="Digite seu email" />
+                            </label>
+                            <label>
+                                <i className="fi fi-sr-lock icon-modify"></i>
+                                <input type="password" name="senha" placeholder="Digite sua senha" />
+                            </label>
+                            <a href="#" className={Styles.password}>Esqueceu a senha?</a>
+                            <button type="button" className={`${Styles.segundo_botao} ${Styles.botao}`}>Entrar</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Login
+export default Login;
